7ba949d60576e98636aaeb029ba2c1eaf85946a8
Kamil Dydo <k.dydo@merixstudio.com>
2020-06-05 20:40:49 +0200

    added infinite scroll component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import EndlessScroll from "./EndlessScroll";
+import logo from "./logo.svg";
+import "./App.css";
+
+function fetchData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(
+        Array.from(Array(10).keys()).map((_, idx) => idx * Math.random())
+      );
+    }, 1000);
+  });
+}
 
 function App() {
+  const [elements, setElements] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  async function fetch() {
+    setIsLoading(true);
+    const data = await fetchData();
+
+    setElements((curr) => [...curr, ...data]);
+    setIsLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EndlessScroll
+      onReachBottom={fetch}
+      isLoading={isLoading}
+      hasMore={hasMore}
+    >
+      {elements.map((element) => (
+        <p>{element}</p>
+      ))}
+    </EndlessScroll>
   );
 }
 
