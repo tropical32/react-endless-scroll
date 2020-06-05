1a47dc065def29bb8f09dc89b4572080ba550d08
Kamil Dydo <k.dydo@merixstudio.com>
2020-06-05 20:51:28 +0200

    modified package.json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useRef, useEffect } from "react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export function EndlessScroll({
+  onReachBottom = () => {},
+  isLoading = false,
+  hasMore = false,
+  children = null,
+  threshold = 0.8,
+  topWrapperClassName = "endless-scroll-top-wrapper",
+  middleWrapperClassName = "endless-scroll-middle-wrapper",
+  bottomWrapperClassName = "endless-scroll-bottom-wrapper",
+}) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const container = useRef();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  useEffect(() => {
+    const options = {
+      threshold: [threshold],
+    };
+
+    const callback = (entries) => {
+      const [first] = entries;
+
+      setIsIntersecting(first.isIntersecting);
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+    const containerCurrent = container.current;
+
+    observer.observe(containerCurrent);
+
+    return () => observer.unobserve(containerCurrent);
+  }, [threshold]);
+
+  useEffect(() => {
+    if (!isLoading && hasMore && isIntersecting) {
+      onReachBottom();
+    }
+    // eslint-disable-next-line
+  }, [hasMore, isLoading, isIntersecting]);
+
+  return (
+    <div className={topWrapperClassName}>
+      {children}
+      <div
+        ref={container}
+        className={middleWrapperClassName}
+        style={{ position: "relative" }}
+      >
+        <div
+          ref={container}
+          className={bottomWrapperClassName}
+          style={{
+            bottom: "0px",
+            height: "100px",
+            opacity: "50%",
+            position: "absolute",
+            width: "100vw",
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default EndlessScroll;
